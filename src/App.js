8ebcf61755d5e3b57733217013dacaf57e2338fd
Shashank Srivastava <54381338+shashank1503-cipher@users.noreply.github.com>
2022-06-05 16:03:47 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,40 +1,83 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   ChakraProvider,
-  Box,
+  Flex,
+  Heading,
+  extendTheme,
   Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
+  useColorModeValue,
+  Button,
+  Spinner,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
-
+import '@fontsource/raleway/400.css';
+import '@fontsource/poppins/700.css';
 function App() {
+  const customTheme = {
+    styles: {
+      global: () => ({
+        body: {
+          bg: '#121212',
+        },
+      }),
+    },
+  };
+  const theme = extendTheme(customTheme);
+  const [joke, setJoke] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const getJoke = async () => {
+    setLoading(true);
+    setError(false);
+    try {
+      const res = await fetch(
+        'https://v2.jokeapi.dev/joke/Programming?type=single'
+      );
+      const data = await res.json();
+      setJoke(data.joke);
+    } catch (error) {
+      setError(true);
+    }
+    setLoading(false);
+  };
+  useEffect(() => {
+    getJoke();
+  }, []);
+
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
+      <Flex justifyContent={'space-between'} p={5}>
+        <Heading size="lg" color="#21E6C1" fontFamily={`'Poppins', sans-serif`}>
+          Punbytes
+        </Heading>
+      </Flex>
+      {loading ? (
+        <Flex
+          minH={'75vh'}
+          fontSize={'2xl'}
+          justifyContent={'center'}
+          alignItems={'center'}
+        >
+          <Spinner color="#21E6C1" size={'xl'} />
+        </Flex>
+      ) : (
+        <>
+          <Flex justifyContent={'center'} alignItems={'center'} minH={'75vh'}>
+            <Text
+              fontSize={'3xl'}
+              textAlign="center"
+              color="#21E6C1"
+              fontFamily={`'Raleway', sans-serif`}
             >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+              {joke}
+            </Text>
+          </Flex>
+          <Flex justifyContent={'flex-end'} mr={10}>
+            <Button onClick={getJoke}> Another One</Button>
+          </Flex>
+        </>
+      )}
     </ChakraProvider>
   );
 }
